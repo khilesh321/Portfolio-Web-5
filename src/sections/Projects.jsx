cf412a0acb264d82b8e46d0110d56cd23ffa28dc
Khilesh Jawale <khilujawale321@gmail.com>
2025-07-11 19:06:58 +0530

    Replace border with ring on project preview image for improved styling

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -21,7 +21,7 @@ function Projects() {
       <h2 className="text-heading">My Selected Projects</h2>
       <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full"></div>
       {myProjects.map(project => <Project key={project.id} {...project} setPreview={setPreview}/>)}
-      {preview && !isSmallScreen && <motion.img className="fixed top-0 left-0 z-50 object-cover h-56 rounded-lg border-2 border-neutral-500 pointer-events-none"
+      {preview && !isSmallScreen && <motion.img className="fixed top-0 left-0 z-50 object-cover h-56 rounded-lg ring-2 ring-neutral-500 pointer-events-none"
         style={{x: springX, y: springY}}
         src={preview}
       />}
