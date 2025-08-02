2bc78844cf4f4009c02b269b12f9bbd2d0dc7d44
Khilesh Jawale <khilujawale321@gmail.com>
2025-08-02 13:26:04 +0530

    Fix: Change onMouseEnter to onMouseMove for project preview display

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -28,7 +28,7 @@ function Project({title, description, subDescription, href, image, tags, setPrev
 
   return (<>
     <div ref={projectRef} className="flex-wrap items-center justify-between py-10 space-y-14 sm:flex sm:space-y-0"
-      onMouseEnter={() => setPreview(image)}
+      onMouseMove={() => setPreview(image)}
       onMouseLeave={() => setPreview(null)}
     >
       <div>
